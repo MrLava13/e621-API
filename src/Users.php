d0ef70aea4df2ebbcfeb605f2c7edcda9c8ad003
MrLava13 <mrlava13@simplem.net>
2021-09-12 08:28:32 -0600

    Add User API

---
+++ src/Users.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace e621;
+
+use \e621\process\GET;
+use Error;
+
+class Users
+{
+    private static $url = 'https://e621.net/users.json';
+
+    /**
+     * Fetch the page of tags
+     * 
+     * @param int $page The page number to get the tags from
+     * @param array $options The options to search for
+     * @return returnObject
+     */
+
+    public static function page(int $page, array $options = [])
+    {
+        return new returnObject(GET::s(static::$url, array_merge($options, ['page' => $page])));
+    }
+
+    /**
+     * Fetch a listing of tags from a given id
+     * 
+     * @param int $id The id to search from
+     * @param string $sort The method of searching, `a` for after, or `b` for before
+     * @param array $options The options to search for
+     * @return returnObject
+     */
+
+    public static function id(int $id, string $sort = 'a', array $options = [])
+    {
+        if (!in_array($sort, ['a', 'b']))
+            throw new Error('Sort can only be `a` or `b`');
+        return new returnObject(GET::s(static::$url, array_merge($options, ['page' => $sort . $id])));
+    }
+}
